84217d4397c9265cbcc8cb7ac24f9a2fb3d41787
Vubaphuc <112857123+Vubaphuc@users.noreply.github.com>
2023-03-27 13:11:34 +0700

    update
    update

---
+++ doan-backend/src/main/java/com/example/doanbackend/DoanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.doanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoanBackendApplication.class, args);
+    }
+
+}
